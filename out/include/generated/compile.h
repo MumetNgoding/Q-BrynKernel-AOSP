f9f4e3d4d7b25f24e8a88c20bd26a8af9646408a
MumetNgoding <nugroho31.com@gmail.com>
2020-07-05 00:25:30 +0000

    BrynKernel-Q

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jul 4 23:21:34 UTC 2020"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "vpsku-semoga-awet-ya"
+#define LINUX_COMPILER "Android (6607189 based on r383902c) clang version 11.0.3 (https://android.googlesource.com/toolchain/llvm-project b397f81060ce6d701042b782172ed13bee898b79)"
