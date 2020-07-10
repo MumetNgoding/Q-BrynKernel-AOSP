bea5cfc4e299fac7b4cf8403775713159ecd9540
MumetNgoding <nugroho31.com@gmail.com>
2020-07-10 11:23:02 +0000

    Fastchg and FpBoost

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#4 SMP PREEMPT Fri Jul 10 06:37:21 UTC 2020"
+#define UTS_VERSION "#5 SMP PREEMPT Fri Jul 10 11:15:02 UTC 2020"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "vpsku-semoga-awet-ya"
 #define LINUX_COMPILER "Android (6607189 based on r383902c) clang version 11.0.3 (https://android.googlesource.com/toolchain/llvm-project b397f81060ce6d701042b782172ed13bee898b79)"
