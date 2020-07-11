d8a45453a1edc57f03404605023cdb5a5dac6923
MumetNgoding <nugroho31.com@gmail.com>
2020-07-11 13:32:41 +0000

    Merge WLAN CAF Tag LA.UM.8.6.2.r1-07800-89xx.0, Improve Good Audio & Touch Boost

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 10 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#2 SMP PREEMPT Sat Jul 11 09:40:50 UTC 2020"
+#define UTS_VERSION "#10 SMP PREEMPT Sat Jul 11 13:23:48 UTC 2020"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "vpsku-semoga-awet-ya"
 #define LINUX_COMPILER "Android (6607189 based on r383902c) clang version 11.0.3 (https://android.googlesource.com/toolchain/llvm-project b397f81060ce6d701042b782172ed13bee898b79)"
