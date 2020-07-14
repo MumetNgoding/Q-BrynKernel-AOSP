c6a5e581bd4060354415997ef81f5472ed548472
MumetNgoding <nugroho31.com@gmail.com>
2020-07-14 07:26:16 +0000

    Clean

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 11 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#11 SMP PREEMPT Sun Jul 12 02:42:03 UTC 2020"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "vpsku-semoga-awet-ya"
-#define LINUX_COMPILER "Android (6607189 based on r383902c) clang version 11.0.3 (https://android.googlesource.com/toolchain/llvm-project b397f81060ce6d701042b782172ed13bee898b79)"
